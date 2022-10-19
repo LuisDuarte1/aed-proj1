6facad0512b7d739839a1d74f4c64b9df6197567
Luís Duarte <luis.duarte.2003.13@hotmail.com>
2022-10-19 16:53:38 +0100

    Adiciona número de estudantes à turma

--- src/turma.h
+++ src/turma.h
@@ -11,6 +11,10 @@ class Turma{
     std::string class_code;
     std::string uc_code;
 
+    int estudantes; //Atenção! esta variavel deve ser atualizada quando um aluno é adicionado a ou removido a esta
+    
+
+
 };
 
 #endif
