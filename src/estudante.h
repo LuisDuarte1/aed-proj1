a6f5b1ca555082a9ebbd025fde833f2dc92adf52
Luís Duarte <luis.duarte.2003.13@hotmail.com>
2022-10-23 15:33:19 +0100

    Adiciona comparação entre estudantes, funções para adicionar e remover turmas inscritas

--- src/estudante.h
+++ src/estudante.h
@@ -11,6 +11,74 @@ class Estudante{
     std::string nome;
     std::list<Turma> turmas_inscrito;
 
+    public:
+        /**
+         * @brief Construtor para um novo objeto estudante
+         * 
+         * @param _numero_up Novo número_up que não deve ser duplicado
+         * @param _nome Novo nome do estudante
+         */
+        Estudante(int _numero_up, std::string _nome);
+
+        /**
+         * @brief Função para comparar dois estudantes. Usa só o número_up para o fazer, já que este deve ser único.
+         * Complexidade: O(1)
+         * 
+         * @param a Estudante para comparar
+         * @return true Se número up for igual.
+         * @return false Se número up for diferente
+         */
+        bool operator==(const Estudante& a);
+
+        /**
+         * @brief Função para ver se o número de estudante é menor em relação a outro estudante.
+         * Principalmente usada para o std::set
+         * Complexidade: O(1)
+         * 
+         * @param a Estudante
+         * @param b Outro estudante
+         * @return true se número_up do a for menor do que número_up do b
+         * @return false se número_up do a for maior do que número_up do b
+         */
+        friend bool operator<(const Estudante& a, const Estudante& b);
+
+        /**
+         * @brief Vai buscar a este Objeto Estudante o respetivo número_up
+         * Complexidade: O(1)
+         * 
+         * @return int 
+         */
+        inline int getStudentNumber() const {return numero_up;};
+
+
+
+        /**
+         * @brief Vai buscar uma cópia da lista de turmas em que o estudante está inscrito.
+         * Complexidade: O(1)
+         * 
+         * @return std::list<Turma> 
+         */
+        inline std::list<Turma> getTurmas() const{ return turmas_inscrito;};
+
+        /**
+         * @brief Adiciona uma turma em que o estudante vai passar a estar inscrito.
+         * Esta função não faz checks se há sobreposições ou se a turma é válida ou até se está duplicada.
+         * Complexidade: O(n)
+         * 
+         * @param nova_turma Turma para adicionar
+         */
+        void adicionarTurma(const Turma& nova_turma);
+
+        /**
+         * @brief Tenta remover uma turma em que o estudante está inscrito. 
+         * Se não for sucedido, dá print para a consola (por agora). 
+         * 
+         * @param turma_remover Turma para remover
+         */
+        void removerTurma(Turma& turma_remover);
+
+
+
 };
 
 #endif
