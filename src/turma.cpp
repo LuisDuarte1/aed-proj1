def48f40fb91c90325a2346cee98d1b93f583f76
andreiasilva66 <up202108769@g.uporto.pt>
2022-10-24 18:24:14 +0100

    add das funcoes para adicionar estudantes e remover; contrutores de pedidos e declaracao de algumas funcoes la

--- src/turma.cpp
+++ src/turma.cpp
@@ -4,3 +4,13 @@
 bool Turma::operator==(const Turma& outra_turma){
     return this->class_code == outra_turma.class_code && this->uc_code == outra_turma.uc_code;
 }
+
+int Turma::getestudantes(){
+    return this->estudantes;
+}
+void Turma::addestudantes(){
+    this->estudantes++;
+}
+void Turma::removeestudantes(){
+    this->estudantes--;
+}
